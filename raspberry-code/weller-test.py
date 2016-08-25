57429d7a2f6e01ed1a37026bca5f35fa47115a53
Tomer Weller <tomer.weller@gmail.com>
2016-08-25 14:47:11 -0400

    oo crap

--- raspberry-code/weller-test.py
+++ raspberry-code/weller-test.py
@@ -1,26 +1,41 @@
-import pygame.mixer        			 # bring in mixing capabilities
+import pygame.mixer  # bring in mixing capabilities
 import time
-import RPi.GPIO as GPIO				 # define
+import RPi.GPIO as GPIO  # define
 
-GPIO.setmode(GPIO.BCM)				 # To use the pin names on the RPi3
-GPIO.setup(23, GPIO.IN) 			 # Setup the pins from the Arduino for input
+GPIO.setmode(GPIO.BCM)  # To use the pin names on the RPi3
+GPIO.setup(23, GPIO.IN)  # Setup the pins from the Arduino for input
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
-pygame.mixer.init(22050, -16, 4) 	 # Begins the pygame mixer module
+pygame.mixer.init(22050, -16, 4)  # Begins the pygame mixer module
 
-chan1 = pygame.mixer.find_channel()
-chan1.play(pygame.mixer.Sound('music-files/boston1.wav'), -1)
-chan2 = pygame.mixer.find_channel()
-chan2.play(pygame.mixer.Sound('music-files/boston2.wav'), -1)
-chan3 = pygame.mixer.find_channel()
-chan3.play(pygame.mixer.Sound('music-files/boston3.wav'), -1)
+class Track:
+    def __init__(self, path):
+        self.chan = pygame.mixer.find_channel()
+        self.chan.play(pygame.mixer.Sound(path), -1)
+        self.target_volume = 0
+
+    def tick(self, target_volume):
+        current_volume = self.chan.get_volume()
+        delta = 0.1 if current_volume < target_volume else -0.1 if current_volume > target_volume else 0
+        self.chan.set_volume(current_volume + delta)
+
+    def get_volume(self):
+        return self.chan.get_volume()
+
+    def stop(self):
+        self.chan.stop()
+
+track1 = Track('music-files/boston1.wav')
+track2 = Track('music-files/boston2.wav')
+track3 = Track('music-files/boston3.wav')
 
 while True:
-    chan1.set_volume(1 if GPIO.input(23) else 0)
-    chan2.set_volume(1 if GPIO.input(24) else 0)
-    chan3.set_volume(1 if GPIO.input(25) else 0)
+    track1.tick(1 if GPIO.input(23) else 0)
+    track2.tick(1 if GPIO.input(24) else 0)
+    track3.tick(1 if GPIO.input(25) else 0)
+    print track1.get_volume(), track2.get_volume(), track3.get_volume()
     time.sleep(0.1)
 
-chan1.stop()
-chan2.stop()
-chan3.stop()
+track1.stop()
+track1.stop()
+track1.stop()
